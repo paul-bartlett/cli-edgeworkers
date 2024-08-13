1790b299e13d509b7ca7e74e9a8df060427e923b
Paul Bartlett <bartlett_paul@hotmail.com>
2024-08-13 13:28:28 -0400

    EDB-3567: Revert to ewids field for old workflow

--- src/edgekv/ekv-response.ts
+++ src/edgekv/ekv-response.ts
@@ -147,7 +147,7 @@ export function logToken(token, savePath = false) {
     console.log(
       'Token Name:          ', token['name'] + '\n'
       + 'CpCode used:         ', token['cpc'] + '\n'
-      + 'Valid for EWIDs:     ', token['restrictToEdgeWorkerIds'] + '\n'
+      + 'Valid for EWIDs:     ', token['ewids'] + '\n'
       + 'Valid on Production: ', production + '\n'
       + 'Valid on Staging:    ', staging + '\n'
       + `Issue date:           ${weekday[issueDate.getDay()]},${issueDate.getDate()} ${shortMnthNames[issueDate.getMonth()]} ${issueDate.getFullYear()} \n`
